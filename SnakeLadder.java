29f4736bdce6c514b814233a5f3f2998301c130c
AV-NASH <avinash21213434@gmail.com>
2020-09-25 21:55:08 +0530

    Dice feature added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,14 +1,24 @@
 package snakeladderpackage;
 
+import java.util.Scanner;
+
 public class SnakeLadder {
  public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
 	int start_position=0;
 	int position;
+	int dice_roll;
 	 
 	System.out.println("wlecome to the snake n ladder game\n");
 	position=start_position;
+	
+	System.out.println("Press enter to roll the  dice");
+	sc.nextLine();
+	dice_roll=(int)((Math.random()*10)%6+1);
+	System.out.println("Dice shows up "+dice_roll);
 	System.out.println("Your starting position is:"+position);
 	
 	
 }
 }
+
