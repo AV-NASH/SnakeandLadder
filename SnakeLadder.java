cea459dfeaa65ad9474f7dc1f01a60b44e652fed
AV-NASH <avinash21213434@gmail.com>
2020-09-25 14:31:56 +0530

    Starting position added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,14 @@
 package snakeladderpackage;
 
 public class SnakeLadder {
-
+ public static void main(String[] args) {
+	int start_position=0;
+	int position;
+	 
+	System.out.println("wlecome to the snake n ladder game\n");
+	position=start_position;
+	System.out.println("Your starting position is:"+position);
+	
+	
+}
 }
